ba91342f1a1a4c4ef37cddc631cfdc1329a7e247
btj-fyi <me@btj.fyi>
2024-10-19 22:25:36 -0700

    Remove unused code

--- Windows/PowerShell/uninstall_unwanted_apps.ps1
+++ Windows/PowerShell/uninstall_unwanted_apps.ps1
@@ -15,22 +15,6 @@ $datetime   = Get-Date -f 'yyyyMMddHHmmss'
 $filename   = "Transcript-${script_name}-${datetime}.log"
 Start-Transcript -Path (Join-Path $log_path -ChildPath $filename)
 
-$registry_path = "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Start\HideShutDown"
-$name         = "value"
-$value        = "1"
-If (-NOT (Test-Path $registry_path)) {
-  New-Item -Path $registry_path -Force | Out-Null
-}  
-New-ItemProperty -Path $registry_path -Name $name -Value $value -PropertyType DWORD -Force 
-
-Stop-Transript
-
-$script_name = $MyInvocation.MyCommand.Name
-$log_path = ("C:\level_automation_logs\${script_name}") -Replace '.ps1', ""
-$datetime   = Get-Date -f 'yyyyMMddHHmmss'
-$filename   = "Transcript-${script_name}-${datetime}.log"
-Start-Transcript -Path (Join-Path $log_path -ChildPath $filename)
-
 #$unwanted_win32_app_names = @('GoTo Opener', 'Zoom(64bit)', 'Teams Machine-Wide Installer', 'Zoom', 'Adobe Acrobat (64-bit)', 'Adobe Refresh Manager')
 Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -in $unwanted_win32_app_names } | ForEach-Object { $_.Uninstall() }
 
