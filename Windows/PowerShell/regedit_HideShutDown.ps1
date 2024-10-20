a563ed37b0e50814c7e2e10bacf568a866c4f50d
btj-fyi <me@btj.fyi>
2024-10-19 22:17:31 -0700

    Add comment-based help section and Transcript logging

--- Windows/PowerShell/regedit_HideShutDown.ps1
+++ Windows/PowerShell/regedit_HideShutDown.ps1
@@ -1,3 +1,20 @@
+<#
+.DESCRIPTION
+    Hide "Shut down" option from Windows Start menu  
+.LANGUAGE
+    PowerShell
+.TIMEOUT
+    100
+.LINK
+    https://github.com/awallaby/scripts/blob/main/Windows/PowerShell/regedit_HideShutDown.ps1
+#>
+
+$script_name = $MyInvocation.MyCommand.Name
+$log_path = ("C:\level_automation_logs\${script_name}") -Replace '.ps1', ""
+$datetime   = Get-Date -f 'yyyyMMddHHmmss'
+$filename   = "Transcript-${script_name}-${datetime}.log"
+Start-Transcript -Path (Join-Path $log_path -ChildPath $filename)
+
 $registry_path = "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Start\HideShutDown"
 $name         = "value"
 $value        = "1"
